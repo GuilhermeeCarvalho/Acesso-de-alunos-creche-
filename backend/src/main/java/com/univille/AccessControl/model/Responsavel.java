b57aabf50feef7317faa5241137c66a32f4fd1cb
GuilhermeeCarvalho <carvalhoguilhermee120@gmail.com>
2026-03-21 12:09:51 -0300

    adicionado novos parametros e regras Class Responsavel

--- backend/src/main/java/com/univille/AccessControl/model/Responsavel.java
+++ backend/src/main/java/com/univille/AccessControl/model/Responsavel.java
@@ -1,17 +1,21 @@
 package com.univille.AccessControl.model;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class Responsavel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String nome;
+
+    @NotBlank
+    private String telefone;
 }
