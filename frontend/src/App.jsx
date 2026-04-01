8f4545d65f06707035eaf523dfbec564af19731c
Sandriely Maciel <macielsandriely993@gmail.com>
2026-04-01 19:53:27 -0300

    Estrutura das paginas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <div style={{ textAlign: 'center', padding: '50px', fontFamily: 'sans-serif' }}>
-      <h1 style={{ color: '#646cff' }}>Sistema Creche - Acesso Legal 👶</h1>
+      <h1 style={{ color: '#646cff' }}>Sistema Creche</h1>
       <div className="card">
         <p style={{ fontSize: '1.2rem', fontWeight: 'bold' }}>{mensagem}</p>
         <button onClick={testarConexao} style={{ padding: '10px 20px', cursor: 'pointer' }}>
@@ -26,7 +26,7 @@ function App() {
         </button>
       </div>
       <p style={{ marginTop: '30px', color: '#888' }}>
-        Desenvolvido pelo Front-end do Grupo 🚀
+       aaaa
       </p>
     </div>
   )
