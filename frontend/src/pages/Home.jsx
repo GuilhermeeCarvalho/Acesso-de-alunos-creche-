439840914e5ec889d260f342996abc5c49e3f2a8
Gabriel-Pavelski <gabrielpavelski11446@gmail.com>
2026-04-15 19:15:06 -0300

    Botões adicionados para acesso de outras paginas

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Home.css';
 
 function Home() {
@@ -6,6 +7,13 @@ function Home() {
     <div>
       <h1>Página Inicial - Creche</h1>
       <p>Bem-vindo ao sistema de gestão.</p>
+      <nav>
+        <ul>
+          <li><Link to="/cadastro-professor">Cadastro de Professores</Link></li>
+          <li><Link to="/cadastro-alunos">Cadastro de Alunos</Link></li>
+          <li><Link to="/relatorio">Relatório</Link></li>
+        </ul>
+      </nav>
     </div>
   );
 }
