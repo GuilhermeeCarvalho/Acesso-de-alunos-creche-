0c68b6defa36aef1cee30859d64748ee0c45f8b1
Gabriel-Pavelski <gabrielpavelski11446@gmail.com>
2026-05-06 20:17:53 -0300

    alteração frontend

--- frontend/src/pages/cadastro-professor.jsx
+++ frontend/src/pages/cadastro-professor.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './cadastro-professor.css';
 
 function CadastroProfessor() {
   return (
-    <div>
-      <h1>Cadastro de Professores</h1>
-      <form>
-        <input type="text" placeholder="Nome do Professor" />
-        <button type="submit">Salvar</button>
-      </form>
-      <Link to="/login">
-        <button type="button">Login</button>
-      </Link>
+    <div className="cadastro-prof-page">
+      <div className="cadastro-prof-card">
+        <div className="cadastro-prof-header">
+          <h1>Cadastro de Professor</h1>
+          <p>Preencha os campos abaixo para criar sua conta</p>
+        </div>
+        <div className="cadastro-prof-form">
+          <label htmlFor="cadastro-prof-user">Usuário</label>
+          <input id="cadastro-prof-user" type="text" placeholder="Digite seu usuário" />
+    
+          <label htmlFor="cadastro-prof-password">Senha</label>
+          <input id="cadastro-prof-password" type="password" placeholder="Digite sua senha" />
+    
+          <button className="cadastro-prof-button">Cadastrar</button>
+        </div>
+      </div>
     </div>
   );
 }
