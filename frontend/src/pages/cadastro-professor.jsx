6373850faa3f3d2c1c4f7c9ad4ab64270bdf0deb
Gabriel-Pavelski <gabrielpavelski11446@gmail.com>
2026-05-08 20:27:35 -0300

    UI/UX cadastro prof

--- frontend/src/pages/cadastro-professor.jsx
+++ frontend/src/pages/cadastro-professor.jsx
@@ -7,7 +7,7 @@ function CadastroProfessor() {
     <div className="cadastro-prof-page">
       <div className="cadastro-prof-card">
         <div className="cadastro-prof-header">
-          <h1>Cadastro de Professor</h1>
+          <h1>Cadastro de Professores</h1>
           <p>Preencha os campos abaixo para criar sua conta</p>
         </div>
         <div className="cadastro-prof-form">
@@ -18,6 +18,13 @@ function CadastroProfessor() {
           <input id="cadastro-prof-password" type="password" placeholder="Digite sua senha" />
     
           <button className="cadastro-prof-button">Cadastrar</button>
+
+          <div className="cadastro-prof-footer">
+            <span>Já possui conta?</span>
+            <Link to="/login" className="signup-link">
+              Faça login aqui
+            </Link>
+          </div>
         </div>
       </div>
     </div>
